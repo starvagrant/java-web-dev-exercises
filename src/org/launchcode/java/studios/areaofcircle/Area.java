61390de70e6671c58e1d62e1b3239f5d617fdb2e
James Ginns <starvagrant@yahoo.com>
2020-01-06 23:11:39 -0600

    Ensure User Enters a Positive Double.
    Ask the user for a radius, if the entry cannot be turned into
    a double or is not positive, ask again.
    
    Fulfills bonus questions in spirit (Use a do/while loop plus
    user validation).

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -2,14 +2,26 @@ package org.launchcode.java.studios.areaofcircle;
 
 import java.util.Scanner;
 
-import static java.lang.Math.PI;
-
 public class Area {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a radius ");
-        Double radius = input.nextDouble();
-        System.out.println("The area of a circle of radius " + radius + " is : " + Circle.getArea(radius));
+        boolean inputCorrect = true;
+        Double radius = null;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a radius ");
+            String userInput = input.nextLine();
+            try {
+                radius = Double.valueOf(userInput);
+                if (radius < 0) {
+                    throw new NumberFormatException();
+                }
+                inputCorrect = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a positive real numbers only");
+                inputCorrect = false;
+            }
+        } while (!inputCorrect);
+        System.out.println(Circle.getArea(radius));
     }
 }
